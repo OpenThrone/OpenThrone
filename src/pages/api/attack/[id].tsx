36f7ddf54b0b75b1976be814d779b7c55d5100de
Tim G <timgarrity89@gmail.com>
2023-09-11 10:14:46 -0400

    prevent more than 10 attack turns and more than the user has. Part of #4

--- src/pages/api/attack/[id].tsx
+++ src/pages/api/attack/[id].tsx
@@ -12,6 +12,14 @@ export default async function handler(req, res) {
       return res.status(400).json({ status: 'failed' });
     }
 
+    if (req.body.turns > 10) {
+      return res.status(400).json({ status: 'failed' });
+    }
+
+    if (req.body.turns > session.user.turns) {
+      return res.status(400).json({ status: 'failed' });
+    }
+
     return res
       .status(200)
       .json(
