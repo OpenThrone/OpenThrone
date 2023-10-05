9c9316c8a22b8064a4d4276473f4f00a8318345d
Tim G <timgarrity89@gmail.com>
2023-10-04 20:14:52 -0400

    cleaned up the scaling on the attack function

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -38,31 +38,25 @@ export async function attackHandler(attackerId, defenderId, attack_turns) {
         break;
     }
 
-    let fortDamagePercentage = 0;
-    const offenseToDefenseRatio = AttackPlayer.offense / DefensePlayer.defense;
-    if (offenseToDefenseRatio >= 0.9) fortDamagePercentage = 0.1;
-    if (offenseToDefenseRatio >= 1.2) fortDamagePercentage = 0.2;
-    if (offenseToDefenseRatio >= 1.4) fortDamagePercentage = 0.4;
-    if (offenseToDefenseRatio >= 1.6) fortDamagePercentage = 0.6;
-    if (offenseToDefenseRatio >= 1.8) fortDamagePercentage = 0.8;
-    if (offenseToDefenseRatio >= 2) fortDamagePercentage = 1;
+    const offenseToDefenseRatio = AttackPlayer.offense / (DefensePlayer.defense || 1);
+    const scaledOffenseToDefenseRatio = Math.min(Math.max(offenseToDefenseRatio, 0), 2);
+    let fortDamagePercentage = 0.1 * scaledOffenseToDefenseRatio;
 
-    const DmgPerTurn = fortDamagePercentage / 10;
+    const modifiedFortDamagePercentage = fortDamagePercentage * (0.5 + scaledOffenseToDefenseRatio / 4);
+    const DmgPerTurn = modifiedFortDamagePercentage / 10;
+    const maxFortDmg = Math.min(DefensePlayer.fortHitpoints * DmgPerTurn * attack_turns, 40);
 
     if (DefensePlayer.fortHitpoints <= 0) {
       GoldPerTurn *= 1.05;
     }
 
     const isAttackerWinner = AttackPlayer.offense > DefensePlayer.defense;
-
-    // Adjust defender's gold based on GoldPerTurn
     const pillagedGold = DefensePlayer.gold * GoldPerTurn * attack_turns;
 
     if (isAttackerWinner) {
       DefensePlayer.gold -= pillagedGold;
       AttackPlayer.gold += pillagedGold;
 
-      // Update bank_history for pillage
       await prisma.bank_history.create({
         data: {
           gold_amount: pillagedGold,
@@ -76,39 +70,19 @@ export async function attackHandler(attackerId, defenderId, attack_turns) {
       });
     }
 
-    // Adjust defender's fortHitpoints based on DmgPerTurn
-    const fortDmg = DefensePlayer.fortHitpoints * DmgPerTurn * attack_turns;
-    DefensePlayer.fortHitpoints -= fortDmg;
-    // Update the database with the new values (not shown in this code)
+    DefensePlayer.fortHitpoints -= maxFortDmg;
 
     const BaseXP = 1000;
     const LevelDifference = DefensePlayer.level - AttackPlayer.level;
-    const LevelDifferenceBonus =
-      LevelDifference > 0 ? LevelDifference * 0.05 * BaseXP : 0;
-    const FortDestructionBonus =
-      DefensePlayer.fortHitpoints <= 0 ? 0.5 * BaseXP : 0;
+    const LevelDifferenceBonus = LevelDifference > 0 ? LevelDifference * 0.05 * BaseXP : 0;
+    const FortDestructionBonus = DefensePlayer.fortHitpoints <= 0 ? 0.5 * BaseXP : 0;
     const TurnsUsedMultiplier = attack_turns / 10;
 
-    // Calculate XP gained from this attack
     let XP = BaseXP + LevelDifferenceBonus + FortDestructionBonus;
     XP *= TurnsUsedMultiplier;
 
-    // Calculate MaxXP for reference
-    // const MaxXP = (BaseXP + 5 * 0.05 * BaseXP + 0.5 * BaseXP) * TurnsUsedMultiplier;
-
-    // Add the XP to the attacker's experience
     AttackPlayer.experience += XP;
 
-    // Check if the attacker has leveled up
-    /* for (let i = AttackPlayer.level + 1; i <= 16; i++) {
-      if (AttackPlayer.experience >= Levels[i]) {
-        AttackPlayer.level = i;
-      } else {
-        break;
-      }
-    } */
-
-    // Update the attack_log
     const attack_log = await prisma.attack_log.create({
       data: {
         attacker_id: attackerId,
@@ -119,21 +93,19 @@ export async function attackHandler(attackerId, defenderId, attack_turns) {
           offensePoints: AttackPlayer.offense,
           defensePoints: DefensePlayer.defense,
           pillagedGold: isAttackerWinner ? pillagedGold : 0,
-          fortDamage: fortDmg,
+          fortDamage: maxFortDmg,
           xpEarned: XP,
           turns: attack_turns,
         },
       },
     });
 
-    // Update the users in the database with the new values
     await prisma.users.update({
       where: { id: attackerId },
       data: {
         gold: AttackPlayer.gold,
         attack_turns: AttackPlayer.attackTurns - attack_turns,
         experience: AttackPlayer.experience,
-        // level: AttackPlayer.level,
       },
     });
     await prisma.users.update({
@@ -143,12 +115,23 @@ export async function attackHandler(attackerId, defenderId, attack_turns) {
         fort_hitpoints: DefensePlayer.fortHitpoints,
       },
     });
+
     return {
       status: 'success',
       result: isAttackerWinner,
       attacker: AttackPlayer,
       defender: DefensePlayer,
       attack_log: attack_log.id,
+      extra_variables: {
+        'pillagedGold': pillagedGold,
+        'XP': XP,
+        'GoldPerTurn': GoldPerTurn,
+        'levelDifference': levelDifference,
+        'fortDamagePercentage': modifiedFortDamagePercentage,
+        'fortDmg': maxFortDmg,
+        'offenseToDefenseRatio': scaledOffenseToDefenseRatio,
+        'DmgPerTurn': DmgPerTurn,
+      }
     };
   }
   return { status: 'failed' };
