3aa6bb357af8cbdc3b6a461c0850741c202cfddd
Tim G <timgarrity89@gmail.com>
2023-10-04 20:16:13 -0400

    Expanded on the Battle Results page for a more story mode - Story mode provided by gpt

--- src/pages/battle/results/[id].tsx
+++ src/pages/battle/results/[id].tsx
@@ -3,18 +3,51 @@ import { AnimatePresence, motion } from 'framer-motion';
 import prisma from '@/lib/prisma';
 
 const results = ({ battle }) => {
+  const { attackerPlayer, defenderPlayer, winner, stats } = battle;
+  const isPlayerWinner = winner === attackerPlayer.id;
+
   const lines = [];
+  console.log(defenderPlayer)
+  const defenderRace = defenderPlayer?.race ?? 'ELF'; // assuming you have access to the race of the player.
+  console.log(defenderRace);
+  const scenarios = {
+    ELF: {
+      setting: "in the tranquil and ancient forests, home to the Elves",
+      environment: "The leaves rustled, whispering secrets of old, as the air was filled with the melody of nature",
+    },
+    UNDEAD: {
+      setting: "in the forsaken and haunted lands of the Undead",
+      environment: "The air was thick with the stench of decay, and the silence was broken by distant, eerie howls",
+    },
+    GOBLIN: {
+      setting: "in the chaotic and cluttered warrens of the Goblins",
+      environment: "The tunnels echoed with mischievous cackles, and the cluttered terrain was a labyrinth of traps and treasures",
+    },
+    HUMAN: {
+      setting: "in the bustling and fortified kingdoms of Humans",
+      environment: "The banners fluttered in the breeze, and the sound of steel and horses filled the air with anticipation",
+    },
+  };
 
-  lines.push(`Battle ID: ${battle.id}`);
-  lines.push(`Winner ID: ${battle.winner}`);
-  lines.push(
-    `You attacked ${battle.defenderPlayer.display_name} and ${
-      battle.winner === battle.attacker_id ? 'won!' : 'lost!'
-    }`
-  );
-  lines.push(`XP Earned: ${battle.stats.xpEarned}`);
-  lines.push(`Gold Pillaged: ${battle.stats.pillagedGold}`);
+  lines.push(`In a world of mystique and wonder, ${attackerPlayer.display_name} embarked on a journey to ${scenarios[defenderRace].setting}.`);
+  lines.push(`With valor in their heart, ${attackerPlayer.display_name} sought to challenge the might of ${defenderPlayer.display_name}.`);
+  lines.push(scenarios[defenderRace].environment);
+  lines.push(`The battle ensued, a dance of fate and steel, with ${attackerPlayer.display_name} dealing a total fort damage of ${stats.fortDamage}.`);
+  lines.push(`The forces of destiny collided, shaping the tales of heroes and legends.`);
+  lines.push(`When the echoes of battle faded, ${isPlayerWinner ? attackerPlayer.display_name : defenderPlayer.display_name} emerged victorious, a symbol of power and resilience in this epic confrontation!`);
+  lines.push(`${attackerPlayer.display_name} garnered ${stats.xpEarned} experience points and ${isPlayerWinner ? `pillaged ${stats.pillagedGold} gold` : `safeguarded their treasures`} from the ravages of war.`);
+  lines.push(`This saga will traverse through time, a beacon of the valor and spirit of the warriors, a tale sung by bards in the candlelit nights.`);
 
+  const summaryLines = []
+
+  summaryLines.push(`Battle ID: ${battle.id}`);
+  summaryLines.push(`Attacker: ${attackerPlayer.display_name}`);
+  summaryLines.push(`Defender: ${defenderPlayer.display_name}`);
+  summaryLines.push(`Winner: ${isPlayerWinner ? attackerPlayer.display_name : defenderPlayer.display_name}`);
+  summaryLines.push(`XP Earned by Attacker: ${stats.xpEarned}`);
+  summaryLines.push(`Gold Pillaged: ${isPlayerWinner ? stats.pillagedGold.toLocaleString() : 0}`);
+  summaryLines.push(`Fort Damage Dealt by Attacker: ${stats.fortDamage}`);
+  summaryLines.push(`Attack Turns Used: ${stats.turns}`);
   const sentence = {
     hidden: { opacity: 0 },
     visible: {
@@ -58,6 +91,24 @@ const results = ({ battle }) => {
             ))}
           </motion.h3>
         ))}
+        <br />
+        <h2>Battle Summary</h2>
+        {summaryLines.map((line, i) => (
+          <motion.h3
+            className="load-screen--message"
+            variants={sentence}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            key={i}
+          >
+            {line.split('').map((char, index) => (
+              <motion.span key={`${char}-${index}`} variants={letter}>
+                {char}
+              </motion.span>
+            ))}
+          </motion.h3>
+        ))}
       </AnimatePresence>
     </div>
   );
@@ -73,12 +124,14 @@ export const getServerSideProps = async ({ query }) => {
         select: {
           id: true,
           display_name: true,
+          race:true
         },
       },
       defenderPlayer: {
         select: {
           id: true,
           display_name: true,
+          race: true
         },
       },
     },
