18261d9e732eda72c2f3c4a076534c0b2e19ee72
Tim G <timgarrity89@gmail.com>
2023-10-04 20:16:41 -0400

    fix a bug with the attack checking against available attack_turns

--- src/pages/api/attack/[id].tsx
+++ src/pages/api/attack/[id].tsx
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
       return res.status(400).json({ status: 'failed' });
     }
 
-    if (req.body.turns > myUser.turns) {
+    if (req.body.turns > myUser.attack_turns) {
       return res.status(400).json({ status: 'failed' });
     }
 
