43b95aaff59a541c0ba78da205ea6d2aeaaa8f80
Tim G <timgarrity89@gmail.com>
2023-10-04 20:15:32 -0400

    Fort repair logic implemented

---
+++ src/pages/api/repair.ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
+import { Fortifications } from '@/constants';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './auth/[...nextauth]';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Ensure the request is of type POST
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
+
+  // Retrieve session and return an error if not authenticated
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) return res.status(403).json({ error: 'Not authenticated' });
+
+  // Retrieve repair points from the request body and validate
+  const { repairPoints } = req.body;
+  if (repairPoints < 1) return res.status(400).json({ error: 'Invalid repair points' });
+
+  // Retrieve user ID from the session
+  const userId = session.player?.id;
+  if (!userId) return res.status(403).json({ error: 'User ID is missing from session' });
+
+  try {
+    // Find the user and return an error if not found
+    const user = await prisma.users.findUnique({ where: { id: userId } });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    // Retrieve fortification details and calculate total cost for repair
+    const fortification = Fortifications[user.fort_level];
+    const totalCost = repairPoints * fortification.costPerRepairPoint;
+
+    // Return an error if the user does not have enough gold
+    if (user.gold < totalCost) return res.status(400).json({ error: 'Not enough gold' });
+
+    // Calculate new fort hitpoints and ensure it does not exceed maximum allowable hitpoints
+    let newFortHitpoints = user.fort_hitpoints + repairPoints;
+    if (newFortHitpoints > fortification.hitpoints) newFortHitpoints = fortification.hitpoints;
+    
+    // Update the user's gold and fort hitpoints in the database
+    await prisma.users.update({
+      where: { id: userId },
+      data: {
+        gold: user.gold - totalCost,
+        fort_hitpoints: newFortHitpoints,
+      },
+    });
+
+    // Return success response with new gold and fort hitpoints values
+    return res.status(200).json({ success: true, newGold: user.gold - totalCost, newFortHitpoints });
+  } catch (error) {
+    console.error('Error repairing fortification:', error, userId);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
--- src/pages/structures/repair.tsx
+++ src/pages/structures/repair.tsx
@@ -6,12 +6,71 @@ import { useUser } from '@/context/users';
 const Repair = () => {
   const { user, forceUpdate } = useUser();
   const [fortification, setFortification] = useState(Fortifications[0]);
+  const [repairPoints, setRepairPoints] = useState(0);
   useEffect(() => {
     if (user?.fortLevel) {
       setFortification(Fortifications[user.fortLevel]);
     }
   }, [user?.fortLevel]);
-  console.log(user);
+
+  const handleRepair = async (e) => {
+    e.preventDefault(); // Prevent the default form submit
+    try {
+      const response = await fetch('/api/repair', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ repairPoints }),
+      });
+
+      if (!response.ok) {
+        const data = await response.json();
+        // Handle error
+        console.error('Error repairing:', data.error);
+        return;
+      }
+
+      const data = await response.json();
+      // Handle success
+      console.log('Success:', data);
+      forceUpdate(); // Consider calling a function to re-fetch user data after successful repair
+    } catch (error) {
+      // Handle error during fetch
+      console.error('Fetch error:', error);
+    }
+  };
+
+  const handleRepairAll = async () => {
+    const maxRepairPoints = (fortification?.hitpoints ?? 0) - (user?.fortHitpoints ?? 0);
+    if (maxRepairPoints <= 0) return; // No repair needed
+    try {
+      const response = await fetch('/api/repair', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ repairPoints: maxRepairPoints }),
+      });
+
+      if (!response.ok) {
+        const data = await response.json();
+        // Handle error
+        console.error('Error repairing all:', data.error);
+        return;
+      }
+
+      const data = await response.json();
+      // Handle success
+      console.log('Success:', data);
+      forceUpdate(); // Consider calling a function to re-fetch user data after successful repair
+    } catch (error) {
+      // Handle error during fetch
+      console.error('Fetch error:', error);
+    }
+  };
+
+
   return (
     <div className="mainArea pb-10">
       <h2>Fort Repair</h2>
@@ -25,7 +84,7 @@ const Repair = () => {
         </p>
       </div>
 
-      <form className="mb-5">
+      <form className="mb-5" onSubmit={handleRepair}>
         <table className="w-full table-auto">
           <thead>
             <tr>
@@ -51,6 +110,38 @@ const Repair = () => {
                 Total Repair Cost:{' '}
                 {fortification?.costPerRepairPoint *
                   (fortification?.hitpoints - user?.fortHitpoints)}
+              </td>
+            </tr>
+            <tr className="odd:bg-table-odd even:bg-table-even">
+              <td>
+                <label>
+                  Repair Points:
+                  <input
+                    type="number"
+                    min="0"
+                    max={((fortification?.hitpoints ?? 0) - (user?.fortHitpoints ?? 0)) > 0 ? ((fortification?.hitpoints ?? 0) - (user?.fortHitpoints ?? 0)) : 0}
+                    value={repairPoints}
+                    onChange={(e) => setRepairPoints(Number(e.target.value))}
+                    className="shadow appearance-none border rounded w-75 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </label>
+              </td>
+            </tr>
+            <tr className='odd:bg-table-odd even:bg-table-even'>
+              <td>
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Repair
+                </button>
+                <button
+                  type="button"
+                  onClick={handleRepairAll}
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
+                >
+                  Repair All
+                </button>
               </td>
             </tr>
           </tbody>
