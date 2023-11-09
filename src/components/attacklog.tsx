89f2304988fc73794a1555e6299fc519d4b69be3
Tim G <timgarrity89@gmail.com>
2023-11-09 18:47:27 -0500

    Cleaned up AttackLog to be more like DT

--- src/components/attacklog.tsx
+++ src/components/attacklog.tsx
@@ -1,134 +1,90 @@
 import { UnitTypes } from "@/constants";
 
-const AttackLogTable = ({ logs, type }) => {
-  // This helper function takes in the type and level and returns the name of the unit
-  const getUnitName = (type, level) => {
-    const unit = UnitTypes.find(u => u.type === type && u.level === level);
-    return unit ? unit.name : "Unknown";
-  };
+const getUnitName = (type, level) => {
+  const unit = UnitTypes.find(u => u.type === type && u.level === level);
+  return unit ? unit.name : "Unknown";
+};
 
-  if (logs.length > 0) {
-    return (
-      <table className="min-w-full bg-black">
-        <thead>
-          <tr>
-            {type === 'defense' ? (
-              <th className="border-b px-4 py-2">Attacker</th>
-            ) : (
-              <th className="border-b px-4 py-2">Defender</th>
-            )}
-            <th className="border-b px-4 py-2">Winner</th>
-            <th className="border-b px-4 py-2">Stats</th>
-            <th className="border-b px-4 py-2">Casualties</th>
-          </tr>
-        </thead>
-        <tbody>
-          {logs.map((log) => (
-            <tr key={log.id}>
-              {type === 'defense' ? (
-                <td className="border-b px-4 py-2">
-                  {log.attackerPlayer?.display_name || 'Unknown'}<br />
-                  {new Date(log.timestamp).toLocaleString('en-US', {
-                    year: 'numeric',
-                    month: 'numeric',
-                    day: 'numeric',
-                    hour: '2-digit',
-                    minute: '2-digit',
-                    hour12: false,
-                  })}
-                </td>
-              ) : (
-                <td className="border-b px-4 py-2">
-                    {log.defenderPlayer?.display_name || 'Unknown'}<br />
-                    {new Date(log.timestamp).toLocaleString('en-US', {
-                      year: 'numeric',
-                      month: 'numeric',
-                      day: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                      hour12: false,
-                    })}
-                </td>
-              )}
-              
-              <td className="border-b px-4 py-2">
-                {log.winner === log.attacker_id ? 'Attacker' : 'Defender'}
-              </td>
-              <td className="border-b px-4 py-2">
-                <ul>
-                  <li>
-                    Pillaged Gold: {log.stats.pillagedGold.toLocaleString()}
-                  </li>
-                  <li>Offense Points: {log.stats.offensePoints}</li>
-                  <li>Defense Points: {log.stats.defensePoints}</li>
-                  <li>XP Earned: {log.stats.xpEarned}</li>
-                  <li>Fort Damage: {log.stats.fortDamage.toLocaleString()}</li>
-                  <li>Turns Used: {log.stats.turns}</li>
-                </ul>
-              </td>
-              <td className="border-b px-4 py-2">
-                <ul>
-                  <li>Attacker Losses:
-                    {log.stats && log.stats.attacker_losses && Object.keys(log.stats.attacker_losses).length === 0 ? (
-                      "No losses"
-                    ) : (
-                      <ul>
-                        {log.stats && log.stats.attacker_losses && Object.entries(log.stats.attacker_losses).map(([key, value]) => {
-                          const [type, level] = key.split('-');
-                          return (
-                            <li key={key}>
-                              {getUnitName(type, parseInt(level))}: {value}
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    )}
+const formatDate = (timestamp) => {
+  return new Date(timestamp).toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+};
 
-                  </li>
-
-                  <li>Defender Losses:
-                    {log.stats && log.stats.defender_losses && Object.keys(log.stats.defender_losses).length === 0 ? (
-                      "No losses"
-                    ) : (
-                      <ul>
-                        {log.stats && log.stats.defender_losses && Object.entries(log.stats.defender_losses).map(([key, value]) => {
-                          const [type, level] = key.split('-');
-                          return (
-                            <li key={key}>
-                              {getUnitName(type, parseInt(level))}: {value}
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    )}
-
-                  </li>
-
-                </ul>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+const LossesList = ({ losses }) => {
+  if (Object.keys(losses).length === 0) {
+    return "No losses";
   }
+
+  return (
+    <ul>
+      {Object.entries(losses).map(([key, value]) => {
+        const [type, level] = key.split('-');
+        return <li key={key}>{getUnitName(type, parseInt(level))}: {value}</li>;
+      })}
+    </ul>
+  );
+};
+
+const StatsList = ({ stats, type }) => (
+  <ul>
+    <li>Pillaged Gold: {stats.pillagedGold.toLocaleString()}</li>
+    <li>XP Earned: {stats.xpEarned}</li>
+    {type !== 'defense' ? <li>Turns Used: {stats.turns}</li> : ''}
+  </ul>
+);
+
+const PlayerOutcome = ({ log, type }) => (
+  <><td className="border-b px-4 py-2">
+    {type === 'defense' ? (log.winner === log.defender_id ? 'Y' : 'N'):(log.winner === log.attacker_id ? 'Y': 'N')}<br />
+  </td>
+  <td className="border-b px-4 py-2">
+    {type === 'defense' ? log.attackerPlayer?.display_name : log.defenderPlayer?.display_name}  <br />
+    {formatDate(log.timestamp)}
+  </td></>
+);
+
+const AttackLogTable = ({ logs, type }) => {
+  const isEmpty = logs.length === 0;
+  const tableHeaders = type === 'defense' ? ['Outcome', 'Attack from player', 'Pillage and Experience', 'Casualties'] : ['Outcome', 'Attack on player', 'Pillage and Experience', 'Casualties'];
+
   return (
     <table className="min-w-full bg-black">
       <thead>
         <tr>
-          <th className="border-b px-4 py-2">Attacker</th>
-          <th className="border-b px-4 py-2">Defender</th>
-          <th className="border-b px-4 py-2">Time of Attack</th>
-          <th className="border-b px-4 py-2">Winner</th>
-          <th className="border-b px-4 py-2">Stats</th>
+          {tableHeaders.map((header) => (
+            <th key={header} className="border-b px-4 py-2">{header}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td colSpan="5" className="text-center">
-            No battles recorded
-          </td>
-        </tr>
+        {isEmpty ? (
+          <tr>
+            <td colSpan={tableHeaders.length} className="text-center">
+              No battles recorded
+            </td>
+          </tr>
+        ) : (
+          logs.map((log) => (
+            <tr key={log.id}>
+              <PlayerOutcome log={log} type={type} />
+              <td className="border-b px-4 py-2">
+                <StatsList stats={log.stats} type={type} />
+              </td>
+              <td className="border-b px-4 py-2">
+                <ul>
+                  <li>Attacker Losses: <LossesList losses={log.stats.attacker_losses || {}} /></li>
+                  <li>Defender Losses: <LossesList losses={log.stats.defender_losses || {}} /></li>
+                </ul>
+              </td>
+            </tr>
+          ))
+        )}
       </tbody>
     </table>
   );
