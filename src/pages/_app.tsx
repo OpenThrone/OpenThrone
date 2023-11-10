cf18fb6ed49b00fb757679fc2e9a51b8475ecdcf
Tim G <timgarrity89@gmail.com>
2023-11-09 20:47:53 -0500

    Add suspense to the site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import 'rpg-awesome/css/rpg-awesome.min.css';
 
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import Layout from '@/components/Layout'; // Import the Layout component
 import { LayoutProvider } from '@/context/LayoutContext';
@@ -14,15 +14,16 @@ const MyApp = ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) => (
-  <SessionProvider session={session}>
+  <Suspense><SessionProvider session={session}>
     <UserProvider>
       <LayoutProvider>
-        <Layout>
+        <Layout meta={undefined}>
           <Component {...pageProps} />
         </Layout>
       </LayoutProvider>
     </UserProvider>
   </SessionProvider>
+  </Suspense>
 );
 
 export default MyApp;
