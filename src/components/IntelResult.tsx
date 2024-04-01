b407365d5806cdc673c08f86cf99e9363554e382
Tim G <timgarrity89@gmail.com>
2024-03-31 20:52:16 -0400

    cleaned up IntelResult

--- src/components/IntelResult.tsx
+++ src/components/IntelResult.tsx
@@ -9,86 +9,54 @@ const IntelResult = ({ battle, viewerID }) => {
   const isPlayerWinner = winner === viewerID;
   const isAttackerWinner = winner === attackerPlayer.id;
   console.log(stats);
-  const lines = [];
-  const defenderRace = defenderPlayer?.race ?? 'ELF';
-
-  const describeLosses = (losses) => {
-    return Object.entries(losses).map(([key, value]) => {
-      const [unitType, unitLevel] = key.split('-');
-      return `${value} ${unitType} level ${unitLevel}`;
-    }).join(', ');
-  };
-
-  const totalLosses = (losses) => {
-    if (losses)
-      return Object.values(losses).reduce((acc, curr) => acc + curr, 0);
-    else return 0;
-  };
-
-  const attackerTotalLosses = totalLosses(stats.attacker_losses);
-  const defenderTotalLosses = totalLosses(stats.defender_losses);
 
   const summaryLines = []
 
-  const countUnitsOfType = (units, type) => {
-    return units.filter(unit => unit.type === type)
-      .reduce((acc, curr) => acc + curr.quantity, 0);
-  }
-
-  summaryLines.push(`Battle ID: ${battle.id}`);
+  summaryLines.push({ text: `Battle ID: ${battle.id}`, className:"text-xl text-black font-medieval font-bold" });
   if (isViewerAttacker)
-    summaryLines.push(`You sent ${stats.spyResults.spiesSent} ${stats.spyResults.spiesSent > 1 ? 'spies' : 'spy'} to ${defenderPlayer.display_name}`);
+    summaryLines.push({ text: `You sent ${stats.spyResults.spiesSent} ${stats.spyResults.spiesSent > 1 ? 'spies' : 'spy'} to ${defenderPlayer.display_name}`, className:"text-lg text-black font-semibold" });
 
-  summaryLines.push(`${isViewerAttacker ? 'You were' : attackerPlayer.display_name + ' was'} ${isAttackerWinner ? 'successful' : 'unsuccessful.'}`);
+  summaryLines.push({ text: `${isViewerAttacker ? 'You were' : attackerPlayer.display_name + ' was'} ${isAttackerWinner ? 'successful' : 'unsuccessful.'}`, className: `text-2xl ${isAttackerWinner ? 'text-green-900' : 'text-red-600'}` });
   if (isAttackerWinner) {
     if (stats.spyResults.intelligenceGathered.units && stats.spyResults.intelligenceGathered.units.length > 0) {
-      summaryLines.push('Units Found');
+      summaryLines.push({ text: 'Units Found', className:"text-xl text-black font-bold underline"});
       stats.spyResults.intelligenceGathered.units.forEach(unit => {
 
         const uType = unit.type[0] + unit.type.slice(1).toLowerCase();
-        summaryLines.push(`${unit.quantity}x Level ${unit.level} ${uType} Units found`);
+        summaryLines.push({ text: `${unit.quantity}x Level ${unit.level} ${uType} Units found`, className: "text-lg mb-2 text-black font-semibold" });
       });
     }
     if (stats.spyResults.intelligenceGathered.items && stats.spyResults.intelligenceGathered.items.length > 0) {
-      summaryLines.push('Items Found');
+      summaryLines.push({ text: 'Items Found', className: "text-xl text-black font-bold underline" });
       stats.spyResults.intelligenceGathered.items.forEach(item => {
         const iName = ItemTypes.find(i => i.usage === item.usage && i.level === item.level && i.type === item.type).name;
-        summaryLines.push(`${item.quantity}x ${item.usage} ${iName} found`);
+        summaryLines.push({ text: `${item.quantity}x ${item.usage} ${iName} found`, className: "text-lg mb-2 text-black font-semibold" });
       });
     }
   }
-  /*summaryLines.push(`${isViewerAttacker ? 'You' : attackerPlayer.display_name} attacked ${isViewerDefender ? 'You' : defenderPlayer.display_name}`);
-  summaryLines.push(`${isViewerAttacker ? 'Your' : attackerPlayer.display_name}'s ${countUnitsOfType(stats.startOfAttack.Attacker.units, 'OFFENSE')} soldiers did X damage`);
-  summaryLines.push(`${isViewerDefender ? 'Your' : defenderPlayer.display_name}'s ${countUnitsOfType(stats.startOfAttack.Defender.units, 'DEFENSE')} guards did Y damage`);
-  summaryLines.push(`${isPlayerWinner ? 'You' : isAttackerWinner ? attackerPlayer.display_name : defenderPlayer.display_name} won the battle`);
-  summaryLines.push(`${isPlayerWinner ? 'You' : isAttackerWinner ? attackerPlayer.display_name : defenderPlayer.display_name} earned ${stats.xpEarned} XP`)
-  summaryLines.push(`Gold Pillaged: ${isPlayerWinner ? stats.pillagedGold.toLocaleString() : 0}`);
-  summaryLines.push(`Fort Damage Dealt by Attacker: ${stats.forthpAtStart - stats.forthpAtEnd}`);
-  summaryLines.push(`Total Units Lost by Attacker: ${attackerTotalLosses}`);
-  summaryLines.push(`Total Units Lost by Defender: ${defenderTotalLosses}`);
-*/
-  const sentence = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        delayChildren: 1,
-        staggerChildren: 0.06,
-        staggerDirection: -1,
+  
+    const sentence = {
+      hidden: { opacity: 0 },
+      visible: {
+        opacity: 1,
+        transition: {
+          delayChildren: 1,
+          staggerChildren: 0.06,
+          staggerDirection: -1,
+        },
       },
-    },
-    exit: { opacity: 0 },
-  };
+      exit: { opacity: 0 },
+    };
 
-  const letter = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        duration: 0.3,
+    const letter = {
+      hidden: { opacity: 0 },
+      visible: {
+        opacity: 1,
+        transition: {
+          duration: 0.3,
+        },
       },
-    },
-  };
+    };
 
   return (
     <div>
@@ -127,15 +95,15 @@ const IntelResult = ({ battle, viewerID }) => {
         <AnimatePresence>
           {summaryLines.map((line, i) => (
             <motion.p
-              className="load-screen--message font-medieval font-semibold text-2xl"
+              className={`load-screen--message font-medieval ${line.className}`}
               variants={sentence}
               initial="hidden"
               animate="visible"
               exit="exit"
               key={`${i}-secondanimate`}
             >
-              {line.split('').map((char, index) => (
-                <motion.span key={`${char}-${index}-2`} variants={letter} className='text-lg mb-2 text-black'>
+              {line.text.split('').map((char, index) => (
+                <motion.span key={`${char}-${index}-2`} variants={letter}>
                   {char}
                 </motion.span>
               ))}
