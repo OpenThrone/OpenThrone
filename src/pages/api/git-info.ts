2ac1836acdbc720f5b4ca390074137690bb66466
Tim G <timgarrity89@gmail.com>
2024-04-04 22:22:32 -0400

    add git commit msg to footer

--- src/pages/api/git-info.ts
+++ src/pages/api/git-info.ts
@@ -5,6 +5,9 @@ export default function handler(req, res) {
     // Get the latest commit hash
     const latestCommit = execSync('git rev-parse HEAD').toString().trim();
 
+    // Get the latest commit message
+    const latestCommitMessage = execSync('git log -1 --pretty=format:"%s"').toString().trim();
+
     // Get the number of pending changes
     const modifiedFiles = execSync('git diff --shortstat').toString().trim();
     const untrackedFilesCount = execSync('git ls-files --others --exclude-standard').toString().split('\n').filter(Boolean).length;
@@ -13,7 +16,7 @@ export default function handler(req, res) {
     const pendingChanges = `${modifiedFiles}, ${untrackedFilesCount} untracked files`;
 
     // Send the git information as a response
-    res.status(200).json({ latestCommit, pendingChanges });
+    res.status(200).json({ latestCommit, latestCommitMessage, pendingChanges });
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve git information' });
   }
