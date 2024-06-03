18c4f9a919c933c2de465c12f5ef3096832f3a2c
Tim G <timgarrity89@gmail.com>
2024-06-03 19:41:10 -0400

    #86 fix for the Housing upgrades page

--- src/pages/structures/housing.tsx
+++ src/pages/structures/housing.tsx
@@ -1,11 +1,37 @@
 import { HouseUpgrades } from '@/constants';
 import { useUser } from '@/context/users';
-import { Group, Paper, rem, SimpleGrid, ThemeIcon, Text } from '@mantine/core';
+import { Group, Paper, rem, ThemeIcon, Text } from '@mantine/core';
 import { BiCoinStack, BiSolidBank } from 'react-icons/bi';
-import classes from './housing.module.css'
+import classes from './housing.module.css';
+import { useEffect, useState } from 'react';
+import toLocale from '@/utils/numberFormatting';
 
 const Housing = (props) => {
   const { user } = useUser();
+  const [mounted, setMounted] = useState(false); // Track if component is mounted
+  const [houseLevel, setHouseLevel] = useState(1);
+  const [gold, setGold] = useState(BigInt(0));
+  const [goldInBank, setGoldInBank] = useState(BigInt(0));
+  const [citizens, setCitizens] = useState(0);
+  const [citizensDaily, setCitizensDaily] = useState(0);
+  const [houseUpgrade, setHouseUpgrade] = useState(HouseUpgrades[houseLevel]);
+
+  useEffect(() => {
+    setMounted(true); // Set mounted to true when component mounts
+  }, []);
+
+  useEffect(() => {
+    if (!user) return;
+    setHouseUpgrade(HouseUpgrades[user.houseLevel]);
+    setHouseLevel(user.houseLevel);
+    setGold(user.gold);
+    setGoldInBank(user.goldInBank);
+    setCitizens(user.citizens);
+    setCitizensDaily(HouseUpgrades[user.houseLevel].citizensDaily);
+  }, [user]);
+
+  if (!mounted) return null; // Render nothing on the server
+
   return (
     <div className="mainArea pb-10">
       <h2>Housing</h2>
@@ -19,9 +45,8 @@ const Housing = (props) => {
               Gold On Hand
             </Text>
           </Group>
-
           <Group align="flex-end" gap="xs" mt={10}>
-            <Text>{parseInt(user?.gold?.toString() ?? "0").toLocaleString()}</Text>
+            <Text>{toLocale(gold)}</Text>
           </Group>
         </Paper>
         <Paper withBorder p="sm" radius="md" className={classes.card}>
@@ -34,11 +59,11 @@ const Housing = (props) => {
             </Text>
           </Group>
           <Group align="flex-end" gap="xs" mt={10}>
-            <Text>{parseInt(user?.goldInBank?.toString() ?? "0").toLocaleString()}</Text>
+            <Text>{toLocale(goldInBank)}</Text>
           </Group>
         </Paper>
-        </div>
-      
+      </div>
+
       <div className="my-5 flex justify-center">
         <table className="my-4 w-10/12 table-auto text-white">
           <thead>
@@ -50,7 +75,7 @@ const Housing = (props) => {
             <tr className="odd:bg-table-odd even:bg-table-even">
               Current Housing Level:{' '}
               <span className="text-yellow-600">
-                {HouseUpgrades[user?.houseLevel ?? 1].name}{' '}
+                {houseUpgrade.name}{' '}
               </span>
               <br />
               <sub className="text-gray-400">
@@ -60,13 +85,13 @@ const Housing = (props) => {
             <tr className="odd:bg-table-odd even:bg-table-even">
               New Citizens Per Day:{' '}
               <span className="text-yellow-600">
-                {HouseUpgrades[user?.houseLevel ?? 0].citizensDaily}
+                {houseUpgrade.citizensDaily}
               </span>
               <br />
               <sub className="text-gray-400">
                 Housing brings new citizens to your fortification every day.
                 <br />
-                You will gain a new citizen every day at midnight DC time.
+                You will gain a new citizen every day at midnight OT time.
               </sub>
             </tr>
           </tbody>
@@ -75,4 +100,5 @@ const Housing = (props) => {
     </div>
   );
 };
+
 export default Housing;
