94edc7b881f04693bdca519841858607ae857b5f
Tim G <timgarrity89@gmail.com>
2024-06-23 21:13:22 -0400

    Fix the revalidation environment variable

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+  if (req.query.secret !== process.env.REVALIDATION_SECRET) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
