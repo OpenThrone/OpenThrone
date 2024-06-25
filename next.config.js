7864ff75a62980c26f54a7c27e4e835edaac1b6e
Tim G <timgarrity89@gmail.com>
2024-06-24 22:17:18 -0400

    updated dev env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 
 /* eslint-disable import/no-extraneous-dependencies */
-const webpack = require('webpack');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -28,18 +27,14 @@ module.exports = withBundleAnalyzer({
   },
   poweredByHeader: true,
   trailingSlash: false,
-  basePath: '',
   images: {
     domains: ['assets.openthrone.dev'], 
   },
   publicRuntimeConfig: {
     apiUrl:
       process.env.NODE_ENV === 'development'
-        ? 'http://192.168.4.112:3002/api' // development api
-        : 'https://OpenThrone.dev/api', // production api
+        ? 'https://alpha.openthrone.dev/api' // development api
+        : 'https://openthrone.dev/api', // production api
   },
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
