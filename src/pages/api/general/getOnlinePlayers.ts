ef07ef221b336b286347c17e3c26b27eca80d747
Tim G <timgarrity89@gmail.com>
2024-06-25 21:45:59 -0400

    Remove unneccessary checks on getOnlinePlayers

--- src/pages/api/general/getOnlinePlayers.ts
+++ src/pages/api/general/getOnlinePlayers.ts
@@ -1,31 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { stringifyObj } from '@/utils/numberFormatting';
-import { withAuth } from '@/middleware/auth';
 import prisma from '@/lib/prisma';
-import src from '@tiptap/extension-link';
-import pages from '..';
-import getUser from './getUser';
+
 
 const getOnlinePlayers = async (req: NextApiRequest, res: NextApiResponse) => {
 
   try {
-    const session = req.session;
-    if (!session) {
-      res.status(401).json({ error: 'Not Authorized - No session' });
-      return;
-    }
-    // Fetch the user based on the session's user ID
-    const user = await prisma.users.findUnique({
-      where: {
-        id: typeof(session.user.id) === 'string' ? parseInt(session.user.id) : session.user.id,
-      },
-    });
-
-    if (!user) {
-      res.status(404).json({ error: 'Not Authorized - Account not found' });
-      return;
-    }
-
     const allUsersCounted = await prisma.users.count({ where: { NOT: { id: 0 } } });
     const onlineUsers = await prisma.users.count({ where: { last_active: { gte: new Date(Date.now() - 1000 * 60 * 10) } } });
     const newUsers = await prisma.users.count({ where: { created_at: { gte: new Date(Date.now() - 1000 * 60 * 60 * 24) } } });
@@ -38,4 +17,4 @@ const getOnlinePlayers = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-export default withAuth(getOnlinePlayers);
+export default getOnlinePlayers;
