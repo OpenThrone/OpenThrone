62a4e83322a46e9668ff1af4cf9f46e88bcfa94b
Tim G <timgarrity89@gmail.com>
2024-07-12 23:17:58 -0400

    Add DT known battle upgrades #140

--- src/constants/Battle_Upgrades.tsx
+++ src/constants/Battle_Upgrades.tsx
@@ -37,4 +37,64 @@ export const BattleUpgrades: UnitUpgradeType[] = [
     killingStrength: 50,
     defenseStrength: 150
   },
+  {
+    type: 'DEFENSE',
+    name: 'Catapult',
+    SiegeUpgradeLevel: 6,
+    level: 2,
+    bonus: 1000,
+    cost: 5000000,
+    unitsCovered: 1,
+    minUnitLevel: 2,
+    killingStrength: 250,
+    defenseStrength: 750
+  },
+  {
+    type: 'SPY',
+    name: 'Disguise Clothes',
+    SiegeUpgradeLevel: 6,
+    level: 1,
+    bonus: 200,
+    cost: 100000,
+    unitsCovered: 1,
+    minUnitLevel: 2,
+    killingStrength: 50,
+    defenseStrength: 150
+  },
+  {
+    type: 'SPY',
+    name: 'Informant',
+    SiegeUpgradeLevel: 6,
+    level: 2,
+    bonus: 1000,
+    cost: 5000000,
+    unitsCovered: 1,
+    minUnitLevel: 2,
+    killingStrength: 250,
+    defenseStrength: 750
+  },
+  {
+    type: 'SENTRY',
+    name: 'Guard Dog',
+    SiegeUpgradeLevel: 6,
+    level: 1,
+    bonus: 200,
+    cost: 100000,
+    unitsCovered: 1,
+    minUnitLevel: 2,
+    killingStrength: 50,
+    defenseStrength: 150
+  },
+  {
+    type: 'SENTRY',
+    name: 'Watch Tower',
+    SiegeUpgradeLevel: 6,
+    level: 2,
+    bonus: 1000,
+    cost: 5000000,
+    unitsCovered: 5,
+    minUnitLevel: 2,
+    killingStrength: 250,
+    defenseStrength: 750
+  }
 ];
