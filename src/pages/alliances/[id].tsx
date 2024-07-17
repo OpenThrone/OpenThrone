8b463dd85068cfba7665fc3a7040d0b08cf0d038
Tim G <timgarrity89@gmail.com>
2024-07-17 09:44:26 -0400

    main structure of alliances is coming together.

--- src/pages/alliances/[id].tsx
+++ src/pages/alliances/[id].tsx
@@ -1,9 +1,12 @@
 import Alert from "@/components/alert";
 import { InferGetServerSidePropsType } from "next";
 import prisma from "@/lib/prisma";
-import { Text, Title, Image } from "@mantine/core";
+import { Text, Title, Image, Flex, Paper, SimpleGrid, Grid, Group, BackgroundImage, Container, Table, Button } from "@mantine/core";
+import { useUser } from "@/context/users";
 
 const Index = ({ alliance }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log('alliance loaded', alliance);
+  const { user } = useUser();
   return (
     <div className = "mainArea pb-10" >
       <h2 className="page-title">Alliance Profile</h2>
@@ -18,27 +21,80 @@ const Index = ({ alliance }: InferGetServerSidePropsType<typeof getServerSidePro
         <Text fw='bold' size='xl' c='dimmed'>
           {alliance.motto}
         </Text>
-        <div className="my-4 flex justify-around">
+        <Flex justify='space-between' my='4'>
           <p className="mb-0">Leader: {alliance.leader.display_name}</p>
           <p className="mb-0">Members: {alliance._count.members}</p>
-        </div>
+        </Flex>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <div className="col-span-1">
-          <div className="card-dark">
-            <div className="flex items-center justify-center">
-              <Image
-                src={alliance.avatar}
-                style={{ width: '100%', height: 'auto', marginLeft: 2 }}
-                alt='avatar'
-                width={484}
-                height={484}
-              />
+      <Grid>
+        <Grid.Col span={{ base: 12, sm: 6 }}>
+          <Paper radius={'md'}>
+            <BackgroundImage
+              src={alliance.bannerimg}
+              radius="sm"
+            >
+              <Flex justify='center'>
+                <Image
+                  src={alliance.avatar}
+                  style={{ width: '100%', height: 'auto', marginLeft: 2 }}
+                  alt='avatar'
+                  width={484}
+                  height={484}
+                />
+              </Flex>
+            </BackgroundImage>
+          </Paper>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, sm: 6 }}>
+          <Paper radius={'xs'}>
+            <Group grow>  
+              <Text>
+                  Right side panel - use for actions/navigation like userprofile
+              </Text>
 
-            </div>
-          </div>
-        </div>
-      </div>
+            </Group>
+          </Paper>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <Paper>
+              <Text>
+                Bottom panel - Show Members List if enabled - Refactor this out to its own component
+              </Text>
+              <Text>
+                Members
+              </Text>
+              <Table>
+                <Table.Thead>
+                  <Table.Tr>
+                    <Table.Th>Member</Table.Th>
+                  <Table.Th>Role</Table.Th>
+                  <Table.Th>Gold</Table.Th>
+                  <Table.Th>Experience</Table.Th>
+                  <Table.Th>Actions</Table.Th>
+                  </Table.Tr>
+                </Table.Thead>
+              <Table.Tbody>
+                {alliance.members.map((member) => (
+                  <Table.Tr key={'Member_'+ member.id}>
+                    <Table.Td>{member.user.display_name}</Table.Td>
+                    <Table.Td>{member.role.name}</Table.Td>
+                    <Table.Td>{member.user.gold}</Table.Td>
+                    <Table.Td>{member.user.experience}</Table.Td>
+                    <Table.Td>
+                      <Group grow>
+                        <Button size="sm" variant="light" disabled>View</Button>
+                        {alliance.leader.id === user?.id && (
+                          <Button size="sm" variant="default" disabled>Kick</Button>
+                        )}
+                      </Group>
+                    </Table.Td>
+                  </Table.Tr>
+                ))}
+                </Table.Tbody>
+              </Table>
+          </Paper>
+        </Grid.Col>
+      </Grid>      
     </div>
   );
 
@@ -52,6 +108,7 @@ export const getServerSideProps = async ({ query }) => {
       leader: {
         select: {
           display_name: true,
+          id: true,
         },
       },
       _count: {
@@ -64,6 +121,11 @@ export const getServerSideProps = async ({ query }) => {
           user: {
             select: {
               display_name: true,
+              id: true,
+              race: true,
+              experience: true,
+              gold: true,
+              class: true,
             },
           },
           role: {
