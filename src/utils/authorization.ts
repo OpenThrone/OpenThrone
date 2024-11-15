a77ec91f23b4c86dfddc8147d94c4bd26d2e45d9
Tim G <timgarrity89@gmail.com>
2024-11-14 21:50:41 -0500

    Add an isModerator to tackle moderation concerns later

--- src/utils/authorization.ts
+++ src/utils/authorization.ts
@@ -7,3 +7,13 @@ export const isAdmin = async (userId: number): Promise<boolean> => {
   });
   return !!adminUser;
 };
+
+export const isModerator = async (userId: number): Promise<boolean> => {
+  const moderatorUser = await prisma.permissionGrant.findFirst({
+    where: {
+      user_id: userId,
+      type: 'MODERATOR',
+    },
+  });
+  return !!moderatorUser;
+}
