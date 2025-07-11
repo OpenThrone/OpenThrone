a6b86c1b36bd4f4b11f74036921f88ba09e327cc
Tim G <timgarrity89@gmail.com>
2025-07-10 22:42:14 -0400

    Update attackFunctions.test for mockUserGenerator

--- src/utils/attackFunctions.test.ts
+++ src/utils/attackFunctions.test.ts
@@ -1,45 +1,64 @@
 import { calculateStrength } from './attackFunctions';
 import UserModel from '../models/Users';
+import MockUserGenerator from './MockUserGenerator';
 
+const user = new MockUserGenerator();
+    user.setBasicInfo({
+      email: 'test@example.com',
+      display_name: 'Test User',
+      race: 'HUMAN',
+      class: 'FIGHTER',
+    });
 describe('calculateStrength', () => {
   it('should calculate the strength for offense units', () => {
-    const user = new UserModel(null);
-    user.units = [
+    user.clearItems();
+    user.clearUnits();
+    user.addUnits([
       { type: 'OFFENSE', quantity: 5, level: 1 },
       { type: 'DEFENSE', quantity: 10, level: 1 },
-    ];
-      user.items = [
-        { type: 'WEAPON', level: 1, usage: 'OFFENSE', quantity: 2, name: 'Dagger', bonus: 25, cost: 12500, race: 'ALL', armoryLevel: 1 },
-        { type: 'WEAPON', level: 2, usage: 'OFFENSE', quantity: 1, name: 'Hatchet', bonus: 50, cost: 25000, race: 'ALL', armoryLevel: 2 },
-        { type: 'HELM', level: 1, usage: 'OFFENSE', quantity: 1, name: 'Padded Hood', bonus: 6, cost: 3000, race: 'ALL', armoryLevel: 1 },
-      ];
-    
-  
-      const strength = calculateStrength(user, 'OFFENSE');
-      expect(strength.killingStrength).toBe(106);
-    });
-  
-    it('should calculate the strength for defense units', () => {
-      const user = new UserModel(null);
-      user.units = [
-        { type: 'OFFENSE', quantity: 5, level: 1 },
-        { type: 'DEFENSE', quantity: 10, level: 1 },
-      ];
-      user.items = [
-        { type: 'WEAPON', level: 1, usage: 'DEFENSE', quantity: 2, name: 'Sling', bonus: 25, cost: 12500, race: 'ALL', armoryLevel: 1 },
-        { type: 'WEAPON', level: 2, usage: 'DEFENSE', quantity: 1, name: 'Hatchet', bonus: 50, cost: 25000, race: 'ALL', armoryLevel: 2 },
-        { type: 'HELM', level: 1, usage: 'DEFENSE', quantity: 1, name: 'Padded Hood', bonus: 6, cost: 3000, race: 'ALL', armoryLevel: 1 },
-      ];
-  
-      const strength = calculateStrength(user, 'DEFENSE');
-      expect(strength.killingStrength).toBe(111);
+    ]);
+    user.addItems([
+      { type: 'WEAPON', level: 1, usage: 'OFFENSE', quantity: 2 },
+      { type: 'WEAPON', level: 2, usage: 'OFFENSE', quantity: 1 },
+      { type: 'HELM', level: 1, usage: 'OFFENSE', quantity: 1 },
+    ]);
+    const userModel = new UserModel(user.getUser());
+    const strength = calculateStrength(userModel, 'OFFENSE');
+    expect(strength.killingStrength).toBe(28);
+  });
+
+  it('should calculate the strength for defense units', () => {
+    user.clearItems();
+    user.clearUnits();
+    user.addUnits([
+      { type: 'OFFENSE', quantity: 5, level: 1 },
+      { type: 'DEFENSE', quantity: 10, level: 1 },
+    ]);
+    user.addItems([
+      { type: 'WEAPON', level: 1, usage: 'DEFENSE', quantity: 2 },
+      { type: 'WEAPON', level: 2, usage: 'DEFENSE', quantity: 1 },
+      { type: 'HELM', level: 1, usage: 'DEFENSE', quantity: 1 },
+    ]);
+    const userModel = new UserModel(user.getUser());
+    const strength = calculateStrength(userModel, 'DEFENSE');
+    expect(strength.killingStrength).toBe(32);
+  });
+  it('should handle empty units and items', () => {
+    user.clearItems();
+    user.clearUnits();
+    user.addUnits([]);
+    user.addItems([]);
+    const userModel = new UserModel(user.getUser());
+    const strength = calculateStrength(userModel, 'OFFENSE');
+    expect(strength.killingStrength).toBe(0);
     });
   it('should handle empty units and items', () => {
-    const user = new UserModel(null);
-    user.units = [];
-    user.items = [];
+    user.clearItems();
+    user.clearUnits();
+    const userModel = new UserModel(user.getUser());
 
-    const strength = calculateStrength(user, 'OFFENSE');
-    expect(strength).toBe(0);
+    const strength = calculateStrength(userModel, 'OFFENSE');
+    expect(strength.killingStrength).toBe(0);
+    expect(strength.defenseStrength).toBe(0);
   });
 });
